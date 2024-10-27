dd7fbc9a0cf9c89d32fbd87c44993590cc859c26
miclukacova <miclukacova@gmail.com>
2024-10-27 08:49:42 +0100

    smoothing

--- Smoothing_ML/density_object.R
+++ Smoothing_ML/density_object.R
@@ -1,7 +1,7 @@
 
 ########### Class ##############
 
-density_object <- function(x, h = NULL) {
+my_density <- function(x, h = NULL) {
   structure(
     list(
       x = x,
@@ -18,10 +18,7 @@ kern_dens <- function(x) {
 }
 
 kern_dens <- function(object, h = "CV", binned = FALSE) {
-  if(is.null(h)){
-    h <- object$cv_bw
-  }
-  else if(h == "CV"){
+  if(h == "CV"){
     h <- object$cv_bw
   }
   else if(h == "AMISE"){
@@ -31,4 +28,70 @@ kern_dens <- function(object, h = "CV", binned = FALSE) {
       kern_dens_bin(object$x, h)
     }
   kern_dens1(object$x, h)
+}
+
+plot.density_object <- function(obj, h, p = 1, binned = FALSE) {
+  dens_data <- as.data.frame(kern_dens(obj, h)[1:2])
+  
+  if(binned == TRUE){
+    dens_data_binned <- as.data.frame(kern_dens_bin(obj, h)[1:2])
+    p <- 3
+  }
+  
+  if(p == 1){
+    pp <- ggplot(tibble(x = x), aes(x = obj$x)) + 
+      geom_histogram(bins = 30, fill = "blue", alpha = 0.3, aes(x = obj$x, y = ..density..)) +
+      geom_function(fun = x_dens, alpha = 0.5, aes(color = "True dens"))+
+      geom_line(data = dens_data, aes(x = x, y = y, color = "Est dens"), linetype = "dashed")+
+      scale_color_manual(values = c("True dens" = "blue", "Est dens" = "hotpink"))+
+      labs(title = "Comparison of true and estimated density", x = "x", y = "Density", color = "Density types")
+  }
+  if(p == 2){
+    
+    dens_r <- as.data.frame(density(obj$x, kernel = "epanechnikov", h)[1:2])
+    
+    pp <- ggplot(tibble(x = x), aes(x = obj$x)) + 
+      geom_histogram(bins = 30, fill = "blue", alpha = 0.3, aes(x = obj$x, y = ..density..)) +
+      geom_function(fun = x_dens, alpha = 0.5, aes(color = "True dens"))+
+      geom_line(data = dens_data, aes(x = x, y = y, color = "Est dens"), linetype = "dashed")+
+      geom_line(data = dens_r, aes(x = x, y = y, color = "density"), linetype = "dashed") +
+      scale_color_manual(values = c("True dens" = "blue", "Est dens" = "hotpink", "density" = "green2"))+
+      labs(title = "Comparison of true and estimated density", x = "x", y = "Density", color = "Density types")
+  }
+  
+  if(p == 3){
+    
+    dens_r <- as.data.frame(density(obj$x, kernel = "epanechnikov", h)[1:2])
+    
+    pp <- ggplot(tibble(x = x), aes(x = obj$x)) + 
+      geom_histogram(bins = 30, fill = "blue", alpha = 0.3, aes(x = obj$x, y = ..density..)) +
+      geom_function(fun = x_dens, alpha = 0.5, aes(color = "True dens"))+
+      geom_line(data = dens_data, aes(x = x, y = y, color = "Est dens"), linetype = "dashed")+
+      geom_line(data = dens_r, aes(x = x, y = y, color = "density"), linetype = "dashed") +
+      geom_line(data = dens_data_binned, aes(x = x, y = y, color = "Binned"), linetype = "dashed") +
+      scale_color_manual(values = c("True dens" = "blue", "Est dens" = "hotpink", 
+                                    "density" = "green2", "Binned" = "purple"))+
+      labs(title = "Comparison of true and estimated density", x = "x",
+           y = "Density", color = "Density types")
+  }
+  return(pp)
+  
+}
+
+test_dens <- function(x) {
+  UseMethod("test_dens")
+}
+
+test_dens <- function(obj, level = 1e-3){
+  h <- seq(0.2,1, length.out = 4)
+  for(i in seq_along(h)){
+    dens_data <- as.data.frame(kern_dens(obj, h[i])[1:2])
+    dens_r <- as.data.frame(density(obj$x, kernel = "epanechnikov", h[i])[1:2])
+    
+    test_that("Our kernel density estimate correspond to density", {
+      expect_equal(
+        dens_data$y,
+        dens_r$y,
+        tolerance = level)})
+  }
 }
